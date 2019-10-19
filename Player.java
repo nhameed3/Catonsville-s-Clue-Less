2dafd113834474bfb7dd3f92554f8c31406a26d3
Nauman Hameed <nahameed3@gmail.com>
2019-10-19 17:05:59 -0400

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,145 @@
+public class Player {
+
+    private String userName = "";
+    private String avatar = "";
+    private boolean player1 = false;
+    private boolean accusation = false;
+
+    private int avatarAccuse = 0;
+    private int roomAccuse = 0;
+    private int weaponAccuse = 0;
+
+/******Key for reference*************************
+    // public static final int SCARLET = 0;
+    // public static final int GREEN   = 1;
+    // public static final int MUSTARD = 2;
+    // public static final int PLUM    = 3;
+    // public static final int WHITE   = 4;
+    // public static final int PEACOCK = 5;
+
+    // public static final int STUDY     = 0;
+    // public static final int HALL      = 1;
+    // public static final int LOUNGE    = 2;
+    // public static final int LIBRARY   = 3;
+    // public static final int BILLIARDS = 4;
+    // public static final int DINING    = 5;
+    // public static final int KITCHEN   = 6;
+
+    // public static final int ROPE        = 0;
+    // public static final int PIPE        = 1;
+    // public static final int KNIFE       = 2;
+    // public static final int WRENCH      = 3;
+    // public static final int CANDLESTICK = 4;
+    // public static final int REVOLVER    = 5;
+****************************************************** */
+    public Player() {
+    }
+
+    public Player(String name, int avatarNum, bool player1 ) {
+        setUserName(name); 
+        setPlayer(player1);
+
+        switch(avatarNum){
+            case 0:
+                avatar = "SCARLET"
+            case 1:
+                avatar = "GREEN"
+            case 2:
+                avatar = "MUSTARD"
+            case 3:
+                avatar = "PLUM"
+            case 4:
+                avatar = "WHITE"
+            case 5:
+                avatar = "PEACOCK"
+        }
+        setAvatar(avatar);
+    }
+
+//SETTERS AND GETTERS
+//////////////////////////////////////////////////
+    public String getUserName() {
+        System.out.println("Player::getUserName");
+        return userName;
+    }
+    public void setUserName(String userName) {
+        System.out.println("Player::setUserName");
+        this.userName = userName;
+    }
+
+//////////////////////////////////////////////////
+    public String getAvatar() {
+        System.out.println("Player::getAvatar");
+        return avatar;
+    }
+    public void setAvatar(String avatar) {
+        System.out.println("Player::setAvatar");
+        this.avatar = avatar;
+    }
+
+//////////////////////////////////////////////////
+    public boolean isPlayer1() {
+        System.out.println("Player::isPlayer1");
+        return player1;
+    }
+    public void setPlayer1(boolean player1) {
+        System.out.println("Player::isPlayer1");
+        this.player1 = player1;
+    }
+
+//////////////////////////////////////////////////
+    public int getavatarAccuse() {
+        System.out.println("Player::getavatarAccuse");
+        return avatarAccuse;
+    }
+    public int getroomAccuse() {
+        System.out.println("Player::getroomAccuse");
+        return roomAccuse;
+    }
+    public int getweaponAccuse() {
+        System.out.println("Player::getweaponAccuse");
+        return weaponAccuse;
+    }
+
+    public int setavatarAccuse(int avatarAccuse) {
+        System.out.println("Player::setavatarAccuse");
+        return this.avatarAccuse = avatarAccuse;
+    }
+    public int setroomAccuse(int roomAccuse) {
+        System.out.println("Player::setroomAccuse");
+        return this.roomAccuse = roomAccuse;
+    }
+    public int setweaponAccuse(int weaponAccuse) {
+        System.out.println("Player::setweaponAccuse");
+        return this.weaponAccuse = weaponAccuse;
+    }  
+
+//////////////////////////////////////////////////
+    public boolean getAccusationResult() {
+        System.out.println("Player::getAccusationResult");
+        return accusation;
+    }
+    public void setAccusationResult(bool accusation) {
+        System.out.println("Player::setAccusationResult");
+        accusation = true;
+    }
+
+//METHODS Preconditions: must be players turn
+//////////////////////////////////////////////////
+    public void makeAccusation(){
+        System.out.println("Player::makeAccusation");
+    }
+
+    public void makeGuess(){
+        //Our software uses accuse variables for both guesses and accusations 
+        System.out.println("Player::makeGuess");
+    }
+
+    public void move(){
+        System.out.println("Player::move");
+    }
+
+
+
+
+}
