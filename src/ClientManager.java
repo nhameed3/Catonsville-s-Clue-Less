7f31a3fdff8549b2368c60e73f82f8ddb73770f6
Matthew Smith <52802185+matthewdavidsmith@users.noreply.github.com>
2019-10-27 15:09:04 -0400

    Update comments on ClientManager

--- src/ClientManager.java
+++ src/ClientManager.java
@@ -1,7 +1,13 @@
+/* ClientManager is for managing each connecting client in 1:1 fashion. Server creates this and calls run() 
+on each new connection. Each instance of ClientManager has its own input and output streams as well as String
+name.
+*/
+
 import java.io.*;
 import java.net.*;
 
 public class ClientManager implements Runnable {
+	// store input and output object treams and a String name
 	ObjectOutputStream out;
 	ObjectInputStream in;
 	String name;
@@ -13,9 +19,13 @@ public class ClientManager implements Runnable {
 		this.out = newOut;
 	}
 	
+	// override run() from Runnable
 	public void run() {
 		try
 		{
+			/*
+			For however long as appropriate (disconnection?) we listen for messages.
+			*/
 			while( true ) {
 				Message inMessage = (Message) in.readObject();
 				//troubleshooting message
