04e60a37959228dde045e6bb6a25c235da675237
pdziki <40967455+pdziki@users.noreply.github.com>
2019-11-09 16:24:40 -0500

    Update Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class Deck {
 
@@ -8,6 +9,12 @@ public class Deck {
 	ArrayList<Card> roomCards = new ArrayList<Card>();
 	ArrayList<Card> weaponCards = new ArrayList<Card>();
 	ArrayList<Card> suspectCards = new ArrayList<Card>();
+	ArrayList<Card> solution = new ArrayList<Card>();
+	
+	Random rand = new Random(); 
+	Card roomCard;
+	Card weaponCard;
+	Card suspectCard;
 	
 	public Deck() {
 		
@@ -34,8 +41,63 @@ public class Deck {
 
 	}
 	
+	public MessageCheckSolution checkSolution(MessageAccusation accuse) {
+		
+		ArrayList<Card> incorrect = new ArrayList<Card>();
+		boolean correct = true;
+		if(accuse.getRoom() != solution.get(0)) {
+			incorrect.add(accuse.getRoom());
+			correct = false;
+		}
+		
+		if(accuse.getWeapon() != solution.get(1)) {
+			incorrect.add(accuse.getWeapon());
+			correct = false;
+		}
+		
+		if(accuse.getSuspect() != solution.get(2)) {
+			incorrect.add(accuse.getSuspect());
+			correct = false;
+		}
+			
+		MessageCheckSolution userReturn = new MessageCheckSolution(correct, incorrect);
+		
+		return userReturn;
+
+		
+		
+	}
+	
+	public void createSolution(){
+
+		roomCard  = roomCards.get(rand.nextInt(roomCards.size()));
+		weaponCard = weaponCards.get(rand.nextInt(weaponCards.size()));
+		suspectCard = suspectCards.get(rand.nextInt(suspectCards.size()));
+		solution.add(roomCard);
+		solution.add(weaponCard);
+		solution.add(suspectCard);
+	}
+	
+	public ArrayList<MessageDeal> dealCards(int playerCount) {
+		
+		ArrayList<MessageDeal> dealMessages = new ArrayList<MessageDeal>();
+		
+		for(int i = 1; i<= playerCount; i++) {
+			ArrayList<Card> playerCards = new ArrayList<Card>();
+			for(int cards = 0; cards<=deckCards.size()/playerCount; cards++) {
+				playerCards.add(deckCards.get(0));
+				deckCards.remove(0);
+			}
+			MessageDeal playerHand = new MessageDeal(playerCards);
+			dealMessages.add(playerHand);
+			
+		}
 	
 	
+		
+		return dealMessages;
+		
+	}
 	
 	
 }
