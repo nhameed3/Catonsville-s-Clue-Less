fda3180473db33f56b768552f3a04bf8972043d9
magicianmk <56797690+magicianmk@users.noreply.github.com>
2019-11-09 18:19:30 -0800

    Update MessageMove.java

--- src/MessageMove.java
+++ src/MessageMove.java
@@ -2,5 +2,11 @@
  * 
  */
 public class MessageMove extends Message{
-
+  
+  	public MessageMove(int givenType, int givenPlayer, int genericInt, String genericText) {
+		  this.messageType = givenType;
+		  this.whichPlayer = givenPlayer;
+      this.genericInt = genericInt;
+      this.genericText = genericText;
+    }
 }
